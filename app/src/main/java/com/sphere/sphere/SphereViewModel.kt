85946a8147f616badcab78309363f30f3385c6e3
8BitWonder <25729145+16BitWonder@users.noreply.github.com>
2021-10-16 10:44:05 -0400

    Hotfix for dao & SphereViewModel

--- app/src/main/java/com/sphere/sphere/SphereViewModel.kt
+++ app/src/main/java/com/sphere/sphere/SphereViewModel.kt
@@ -12,7 +12,7 @@ class SphereViewModel(private val repository: SphereRepository): ViewModel() {
     // - We can put an observer on the data (instead of polling for changes) and only update the
     //   the UI when the data actually changes.
     // - Repository is completely separated from the UI through the ViewModel.
-    val allSpheres: LiveData<List<Sphere>> = repository.allSpheres
+    val allSpheres: LiveData<List<Sphere>> = repository.allSpheres.asLiveData()
 
     /**
      * Launching a new coroutine to insert the data in a non-blocking way
--- app/src/main/java/com/sphere/sphere/room_code/SphereRepository.kt
+++ app/src/main/java/com/sphere/sphere/room_code/SphereRepository.kt
@@ -10,7 +10,7 @@ class SphereRepository(private val sphereDao: SphereDao) {
 
     // Room executes all queries on a separate thread.
     // Observed Flow will notify the observer when the data has changed.
-    val allSpheres: LiveData<List<Sphere>> = sphereDao.getAlphabetizedSpheres()
+    val allSpheres: Flow<List<Sphere>> = sphereDao.getAlphabetizedSpheres()
 
     // By default Room runs suspend queries off the main thread, therefore, we don't need to
     // implement anything else to ensure we're not doing long running database work
