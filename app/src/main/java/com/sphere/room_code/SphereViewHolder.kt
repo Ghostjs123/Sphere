8438d376c9b80d5317e48169df89926db18fbdbe
8BitWonder <25729145+16BitWonder@users.noreply.github.com>
2021-10-30 15:32:21 -0400

    Add listeners for delete and load buttons in MySpheres

--- app/src/main/java/com/sphere/room_code/SphereViewHolder.kt
+++ app/src/main/java/com/sphere/room_code/SphereViewHolder.kt
@@ -1,11 +1,15 @@
 package com.sphere.room_code
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.sphere.R
+import kotlinx.android.synthetic.main.recyclerview_item.view.*
+
+val TAG = "SphereViewHolder"
 
 class SphereViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val sphereItemView: TextView = itemView.findViewById(R.id.textView)
@@ -18,6 +22,12 @@ class SphereViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun create(parent: ViewGroup): SphereViewHolder {
             val view: View = LayoutInflater.from(parent.context)
                 .inflate(R.layout.recyclerview_item, parent, false)
+            view.delete_button.setOnClickListener {
+                Log.i(TAG, "delete_button was pressed.")
+            }
+            view.load_button.setOnClickListener {
+                Log.i(TAG, "load_button was pressed.")
+            }
             return SphereViewHolder(view)
         }
     }
